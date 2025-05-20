ba445c45fb741954992f97aa957feb4a60976aef
mahmudul hasan hasib <mahmudulhasanhasib443@gmail.com>
2025-05-20 23:05:27 +0600

    This is the commit for web application

---
+++ Documents/ML end to end/BostonHousingPrice/app.py
@@ -0,0 +1,32 @@
+import pickle
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+## Load model
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))  # Load the LinearRegression model
+scaler = pickle.load(open('scaling.pkl', 'rb'))     # Load the StandardScaler
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    try:
+        data = request.json['data']
+        print(data)
+        # Transform the input data using the scaler
+        new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+        # Make prediction using the regression model
+        output = regmodel.predict(new_data)
+        print(output[0])
+        return jsonify(float(output[0]))
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
