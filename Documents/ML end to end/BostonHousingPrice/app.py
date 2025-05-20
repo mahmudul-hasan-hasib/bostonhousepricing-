535ae9f237f7abce6ca67c137b9cd9201566d55a
mahmudul hasan hasib <mahmudulhasanhasib443@gmail.com>
2025-05-21 01:08:30 +0600

    initial commit

--- Documents/ML end to end/BostonHousingPrice/app.py
+++ Documents/ML end to end/BostonHousingPrice/app.py
@@ -1,4 +1,5 @@
 import pickle
+import json
 from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pandas as pd
@@ -26,7 +27,14 @@ def predict_api():
         return jsonify(float(output[0]))
     except Exception as e:
         return jsonify({'error': str(e)}), 400
-    
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The house price prediction is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
